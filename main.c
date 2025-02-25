06edbb1dedcb0215961f07c6f3f9fcbe3deec0c8
Pierre <PierreMetrickson@protonmail.com>
2025-02-25 09:06:27 +0200

    PC13 LED Output

--- main.c
+++ main.c
@@ -4,10 +4,11 @@
 
 int main(void) {
 
-    RCC->APB2ENR |= 0b10000; // 0b10000
-    GPIOC->CRH &= ~ //'~' - обратная маска
-    GPIOC->CRH &= ~(CPIO_CRH_CNF13 | CGIP_CHR_MODE13); //GPIOC->CRH[23:20]=0000
+    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN; // RCC->APB2ENR |= 0b10000
+    //GPIOC->CRH &= ~ //'~' - обратная маска
+    GPIOC->CRH &= ~(GPIO_CRH_CNF13 | GPIO_CRH_MODE13); //GPIOC->CRH[23:20]=0000
     GPIOC->CRH |= GPIO_CRH_MODE13_0; // GPIOC->CRH[23:20]=001
-    GPIO->ODR = GPIO_ODR_ODR13
+    GPIOC->ODR = GPIO_ODR_ODR13;
+    
 return 0;
 }
