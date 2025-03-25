43b422662508b3138beb78a6e0a60bebf315ddbc
Pierre <PierreMetrickson@protonmail.com>
2025-03-25 10:34:14 +0200

    25.03.2025

--- main.c
+++ main.c
@@ -2,13 +2,68 @@
 #include <stm32f10x.h>
 #include <stdbool.h>
 
-int main(void) {
+void delay_us(uint32_t us){
+    __asm volatile(
+        "push {r0}\r\n"
+        "mov R0, %0\r\n"      //val = (9 * us) for 72Mhz
+        "_loop:\r\n"    //approx. 8ticks/iteration
+	        "cmp R0, #0\r\n" //1
+	        "beq _exit\r\n"      		//1 or 1+P (when condition is True)
+	        "sub R0, R0, #1\r\n" 	//1
+	        "nop\r\n" 				//1 allignment
+	        "b _loop\r\n" 			//1+P (pipeline refill) ~4 cycle
+        "_exit:\r\n"
+	        "pop {r0}\r\n"
+        :: "r"(9*us) // For 72 Mhz
+    );
+}
 
-    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN; // RCC->APB2ENR |= 0b10000
-    //GPIOC->CRH &= ~ //'~' - обратная маска
-    GPIOC->CRH &= ~(GPIO_CRH_CNF13 | GPIO_CRH_MODE13); //GPIOC->CRH[23:20]=0000
-    GPIOC->CRH |= GPIO_CRH_MODE13_0; // GPIOC->CRH[23:20]=001
-    GPIOC->ODR = GPIO_ODR_ODR13;
-    
+
+
+/* Interrupt handler */
+void TIM2_IRQHandler(void) {
+    if (TIM2->SR & TIM_SR_UIF) {
+        if(GPIOC->ODR & GPIO_ODR_ODR13){
+            GPIOC->ODR &= ~GPIO_ODR_ODR13;
+        } else {
+            GPIOC->ODR |= GPIO_ODR_ODR13;
+        }
+    //Clear Interrupt flag
+    TIM2->SR &= ~TIM_SR_UIF;
+    }
+}
+
+
+int main(void) {
+    // int i = 0;
+    // int mask = 8; // 8 = 0b10000 = 0x8 = (1 << 4)
+    // i = i | mask; // i |= mask;
+
+    /* IO PORTS Configuration */
+    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN; // 0b10000=0x10
+    GPIOC->CRH &= ~(GPIO_CRH_CNF13 | GPIO_CRH_MODE13); // GPIOC->CRH[23:20]=0000
+    GPIOC->CRH |= GPIO_CRH_MODE13_0; // GPIOC->CRH[23:20]=0001
+
+    /* TIM2 Configuration */
+    RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+    RCC->APB1RSTR |= RCC_APB1RSTR_TIM2RST;
+    RCC->APB1RSTR &= ~RCC_APB1RSTR_TIM2RST;
+    TIM2->PSC = 4000;
+    TIM2->ARR = 22000;
+    TIM2->DIER |= TIM_DIER_UIE; // Enable Update Interrupt
+    NVIC_ClearPendingIRQ(TIM2_IRQn);
+    NVIC_EnableIRQ(TIM2_IRQn); // Enable IRQ in NVIC
+    TIM2->CR1 |= TIM_CR1_CEN; // Start timer
+    while (1) {
+        __asm volatile ("nop");
+    }
+
+    while(1){
+        GPIOC->ODR &= ~GPIO_ODR_ODR13;
+        delay_us(1000000);
+        GPIOC->ODR |= GPIO_ODR_ODR13;
+        delay_us(1000000);
+    }
+
 return 0;
 }
